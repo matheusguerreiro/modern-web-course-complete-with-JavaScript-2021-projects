4fbc1778e2cffdbebd85dfb463a1f5372632a881
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2021-05-17 21:47:49 -0300

    commit

---
+++ fundamentals/var, let and const.js
@@ -0,0 +1,10 @@
+// o básico sobre variáveis
+
+var a = 3
+let b = 4
+
+// usar o let, de maneira resumida é a forma mais moderna de declarar uma variável
+
+const c = 7
+
+console.log(a, b, c)
---
+++ fundamentals/weak typing.js
@@ -0,0 +1,8 @@
+// tipagem fraca
+
+let a = 'legal'
+console.log(a)
+console.log(typeof a)
+
+a = 3
+console.log(typeof a)
