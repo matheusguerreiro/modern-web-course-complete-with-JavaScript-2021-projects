db9fe8febc318431798e01120ef56c04b2a52b4b
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2021-05-21 16:49:40 -0300

    commit

---
+++ fundamentals/arrays.js
@@ -0,0 +1,17 @@
+// Arrays
+
+const v = [7.7, 8.9, 6.3, 9.2]
+console.log(v[0], v[3])
+
+v[4] = 10
+console.log(v)
+
+console.log(v.length)
+v.push('mgf')
+console.log(v)
+v.pop()
+console.log(v)
+delete v[2]
+console.log(v)
+
+console.log(typeof v)
