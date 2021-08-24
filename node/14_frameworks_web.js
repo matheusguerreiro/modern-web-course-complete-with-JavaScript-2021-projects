949b0a9a26750af2b9602fbd6142f9cbab5efb32
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2021-08-24 12:15:23 -0300

    commit2408211215

---
+++ node/14_frameworks_web.js
@@ -0,0 +1,4 @@
+// frameworks web
+
+// framework = arcabouço -> ele estrutura o projeto, se constroi a aplicação em cima do framework
+// biblioteca != framework -> biblioteca é um conjunto de funcionalidades
